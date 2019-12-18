25b4a77fe6bdbb9131ea8fe5e4fb45b71b185236
SeniorPug <41790789+SeniorPug@users.noreply.github.com>
2019-12-18 18:49:02 +0200

    Add files via upload

---
+++ sha256.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "sha256.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	string input = "Rabotaj";
+	string output1 = sha256(input);
+
+	cout << "sha256('" << input << "'):" << output1 << endl;
+	return 0;
+}
